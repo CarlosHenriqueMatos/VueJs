c3bdf2a28b1b8d3f9063a93b5ac8d0d445470936
Carlos Matos <carlos.1997matos@gmail.com>
2024-07-17 09:37:57 -0300

    Ligação de dados bidirecional(diretiva v modelo)

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -4,6 +4,93 @@
       Curso Vue 3
     </h1>
 
+    <div>
+      One-way data binding <br>
+      Two-way data binding <br>
+      v-model -> formulários
+    </div>
+    
+    <br>
+
+    <div>
+      <label> Inserção de dados </label><br>
+      <input
+      v-model="name"
+      type="text"
+      ><br>
+      {{ name }}
+    </div>
+    
+    <br>
+
+    <div>
+      <label>Sports</label><br>
+      <select v-model="sports">
+        <option value="">Escolha</option>
+        <option value="Data Science">Data Science</option>
+        <option value="Desenvolvimento">Desenvolvimento</option>
+        <option value="Infra">Infra</option>
+      </select><br>
+      {{ sports }}
+    </div>
+    
+    <br>
+
+    <div>
+      <label>Newsletter</label><br>
+      <input
+        v-model="newsletter"
+        type="radio"
+        value="Sim"
+      > Sim
+      <input
+        v-model="newsletter"
+        type="radio"
+        value="Não"
+      > Não
+      <br>
+      {{newsletter}}
+    </div>
+
+    <div>
+      <label>Contrato</label><br>
+      <input
+        v-model="contract"
+        type="checkbox"
+      > Concordo e aceito os termos...
+      <br>
+      {{contract}}
+    </div>
+    
+    <br>
+
+    <div>
+      <label>Cores que você mais gosta</label><br>
+      <input
+        v-model="colors"
+        type="checkbox"
+        value="Azul"
+      > Azul
+      <input
+        v-model="colors"
+        type="checkbox"
+        value="Amarelo"
+      > Amarelo
+      <br>
+      {{colors}}
+    </div>
+    <br>
+    <div>
+      <label>Contrato</label><br>
+      <input
+        v-model="contract"
+        type="checkbox"
+      > Concordo e aceito os termos...
+      <br>
+      {{contract}}
+    </div>
+
+
     <p :class="[pClass]">
       Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker
     </p>
@@ -39,9 +126,14 @@ export default {
   name: 'HelloWorld',
   data(){
     return{classVar:'title',
+          name: 'Informe o seu nome',
+          sports: "",
+          newsletter: '',
+          contract: false,
           pClass: ['text', 'text-home'],
           styleClass:{'color':'white', 'backgroundColor':'black', 'font-size':'20px'},
-          isHome:true
+          isHome:true,
+          colors:[],
     }
   }
 }
